04ac7e1dd587406389384c02d7cdfdec95c1c35c
Arian Abdi <abdiarian2@gmail.com>
2024-01-18 16:49:26 +0330

    bug fixes

--- scripts/update-filkosh-pod.sh
+++ scripts/update-filkosh-pod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Your existing script commands here
-bash <(curl -Ls https://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@raw.githubusercontent.com/arianabdi/filkosh-pod-api/main/scripts/installation.sh --ipv4)
+bash <(curl -Ls https://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@raw.githubusercontent.com/arianabdi/filkosh-pod-api/main/scripts/installation.sh --ipv4)
