5e7a0579088bc95857b053af380d5dac275cd7b2
Arian Abdi <abdiarian2@gmail.com>
2024-01-19 10:14:57 +0330

    bug fixes

--- scripts/update-filkosh-pod.sh
+++ scripts/update-filkosh-pod.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+PROJECT_NAME="filkosh-pod"
+FOLDER_NAME="filkosh-pod-api"
+
+# delete the PM2 project
+pm2 delete $PROJECT_NAME
+
+# Delete the project folder
+rm -rf $FOLDER_NAME
+
 # Check if the access key is provided
 if [ -z "$1" ]; then
     echo "Error: Access key not provided."
